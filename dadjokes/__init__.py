cbd7b61fe76e045cfa51a3f6966a5b40f2d6d41e
Ahmed <ahmedkhalf567@gmail.com>
2020-07-31 23:20:25 +0400

    Add joke method

--- dadjokes/__init__.py
+++ dadjokes/__init__.py
@@ -1,3 +1,14 @@
 __version__ = "1.0.0"
 
 import requests
+
+
+USER_AGENT = f"dadjokes-plus-plus/v{__version__} (https://github.com/ahmedkhalf/dadjokes-plus-plus)"
+HEADERS = {"User-Agent": USER_AGENT, "Accept": "application/json"}
+
+
+def joke():
+    """Fetches a random dad joke."""
+    r = requests.get("https://icanhazdadjoke.com/", headers=HEADERS)
+    return r.json()["joke"]
+
