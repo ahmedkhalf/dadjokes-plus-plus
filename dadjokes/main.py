f0e9e53097b013a71ebbe7460e7b30b347733a39
Ahmed <ahmedkhalf567@gmail.com>
2020-08-01 18:10:03 +0400

    Add egg

--- dadjokes/main.py
+++ dadjokes/main.py
@@ -2,9 +2,30 @@ import argparse
 import dadjokes
 
 
+def egg():
+    print(
+        """
+        ████
+      ██░░░░██
+    ██░░░░░░░░██
+    ██░░░░░░░░██
+  ██░░░░░░░░░░░░██  Congratulations!
+  ██░░░░░░░░░░░░██  You are one of the few people who read my code.
+  ██░░░░░░░░░░░░██  Here is an egg.
+    ██░░░░░░░░██
+      ████████
+    """
+    )
+
+
 def main():
     parser = argparse.ArgumentParser()
-    parser.parse_args()
+    parser.add_argument("--egg", action="store_true", help=argparse.SUPPRESS)
+    args = parser.parse_args()
+
+    if args.egg:
+        egg()
+        return
 
     print(dadjokes.joke())
 
